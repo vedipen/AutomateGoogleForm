f21524ad0c36052464d4cfa7bb3aa10389f8c4ca
Dipen Ved <vedipen@gmail.com>
2018-04-22 18:53:27 +0530

    Ask for Input

--- run.py
+++ run.py
@@ -12,6 +12,24 @@ email += "@somaiya.edu"
 print("Email : "+email)
 password = getpass.getpass('Password : ')
 
+awards = ["Mr. India award (Low attendance)","Lifetime achievement award ( Student of the year)",
+    "Ranchoddas Shamaldas award (Nerd)","Salman Bhai Award (Gets out of trouble easily)",
+    "Munnabhai & circuit award (Best Friends)","Jimmy Shergill (Friendzoned)"]
+
+minimalAwards = ["Mr. India","Lifetime","Ranchoddas","Salman","Munnabhai","Jimmy"]
+
+for i in range(0,len(awards)):
+    print(str(i+1) + " " + awards[i])
+
+award = int(input("Which award do you want to give? (Ex. 1) : "))
+while award < 1 or award > 6 :
+    award = int(input("Which award do you want to give? (Ex. 1) : "))
+award-=1
+
+person = input("Enter the name of the person you want to give (Ex. Ayush Varma): ")
+
+print("Awarding "+ person + " with "+ awards[award])
+
 driver = webdriver.Chrome()
 driver.get("https://goo.gl/axxoza")
 
@@ -23,15 +41,14 @@ inputElement = driver.find_element_by_name("password")
 inputElement.send_keys(password + Keys.RETURN)
 driver.implicitly_wait(10)
 
-# try:
-WebDriverWait(driver, 100).until(EC.title_contains("Farewell Awards!"));
-driver.find_element_by_xpath("//*[contains(text(), 'Mr. India award')]").click()
-personName = driver.find_element_by_xpath("//input[@type='text']")
-personName.send_keys("Ayush Varma")
-time.sleep(10)
-driver.find_element_by_xpath("//*[contains(text(), 'Submit')]").click()
+try:
+    WebDriverWait(driver, 100).until(EC.title_contains("Farewell Awards!"));
+    driver.find_element_by_xpath("//*[contains(text(), '" + minimalAwards[award] + "')]").click()
+    personName = driver.find_element_by_xpath("//input[@type='text']")
+    personName.send_keys(person)
+    # time.sleep(5)
+    driver.find_element_by_xpath("//*[contains(text(), 'Submit')]").click()
+    # time.sleep(5)
 
-# finally:
-    #
-    # driver.quit()
-#
+finally:
+    driver.quit()
