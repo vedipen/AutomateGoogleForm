5107b85bfb56fd240f02336798f73c2a46a1a0fb
Dipen Ved <vedipen@gmail.com>
2018-04-22 17:49:18 +0530

    Complete somaiya login

--- run.py
+++ run.py
@@ -2,31 +2,22 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.common.keys import Keys
 
-# Create a new instance of the Firefox driver
+import getpass
+
+email = input("Enter your Somaiya ID : ")
+email += "@somaiya.edu"
+print("Email : "+email)
+password = getpass.getpass('Password : ')
+
 driver = webdriver.Chrome()
+driver.get("https://goo.gl/axxoza")
 
-# go to the google home page
-driver.get("http://www.google.com")
+inputElement = driver.find_element_by_name("identifier")
+inputElement.send_keys(email + Keys.RETURN)
+driver.implicitly_wait(10)
 
-# the page is ajaxy so the title is originally this:
-print(driver.title)
-
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
-
-# type in the search
-inputElement.send_keys("cheese!")
-
-# submit the form (although google automatically searches now without submitting)
-inputElement.submit()
-
-try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-    # You should see "cheese! - Google Search"
-    print(driver.title)
-
-finally:
-    driver.quit()
+inputElement = driver.find_element_by_name("password")
+inputElement.send_keys(password + Keys.RETURN)
+driver.implicitly_wait(10)
